ae1f630332941f703d9d40dcb2db1ec59151970b
n1c0x <nicolas.renard67@gmail.com>
2024-02-25 18:31:42 +0100

    added mysql database

--- web/data/index.php
+++ web/data/index.php
@@ -1,14 +1,37 @@
 <?php
 $ip_server = $_SERVER['SERVER_ADDR'];
-$ip_name = $_SERVER['HOSTNAME'];
 
 // Printing the stored address 
-echo "<h1>Server IP Address is: $ip_server</h1>"; 
-echo "Container ID is: $ip_name<br>"; 
+echo "<h1>nginx container IP Address is: $ip_server</h1>"; 
 
-// echo "<pre>";
-// print_r($_SERVER);
-// echo "</pre>";
+echo "<h2>Connect to database</h2>";
+// Basic connection settings
+$databaseHost = 'db:3306';
+$databaseUsername = 'fox';
+$databasePassword = 'fox123';
+$databaseName = 'fox-db';
 
+// Connect to the database
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+if(! $mysqli ) {
+    die('Could not connect: ' . mysql_error());
+}
+
+echo 'Connected successfully';
+echo "<hr>";
+// Fetch contacts (in descending order)
+$result = mysqli_query($mysqli, "SELECT * FROM contacts");
+
+while ($row = mysqli_fetch_assoc($result))
+ {
+    echo "<p><b>ID : </b>".$row['id']."<br>";
+    echo "<b>Name : </b>".$row['name']."<br>";
+    echo "<b>Age : </b>".$row['age']."<br>";
+    echo "<b>Email : </b>".$row['email']."<br></p>";
+ }
+
+mysqli_close($mysqli);
+echo 'Connection closed';
 
 ?>
